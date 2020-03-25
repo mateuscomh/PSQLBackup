66a0b722ee57e7c79455666175016586de23e74c
Djangoo <60662558+mateuscomh@users.noreply.github.com>
2020-03-25 12:31:36 -0300

    Atualizacao total revisada

--- bkp_PGSQL_v2.sh
+++ bkp_PGSQL_v2.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #============================================================================================
-#       ARQUIVO:  bkp_PGSQL.sh
+#       ARQUIVO:  bkp_PGSQLv2.sh
 #       DESCRICAO: Verifica ponto de montagem de storage, remove backups com mais de X dias, gera um dump do banco e compacta. Em caso de erro notifica via email.
 #       REQUISITOS: 
-#       - OBRIGATÓRIO o arquivo de variáveis "PARAMETOSBKPBD" disponibilizado no PATH (/usr/bin/PARAMETOSBKPBD).
+#       * HOMOLOGADO:  Em distribuições CentOS 6.x 
+#       - OBRIGATÓRIO o arquivo de variáveis "PARAMETROSBKPBD" disponibilizado no PATH (/usr/bin/PARAMETROSBKPBD).
 #       - OBRIGATÓRIO em caso de discos rígidos/virtualizados serem registrados em /etc/fstab.
 #       - OBRIGATÓRIO caminho de montagem em /mnt para discos e unidades de rede.
-#       - Todos os ajustes de cada cliente deverão ser escritos no arquivo de variáveis PARAMETOSBKPBD.
-#       VERSAO:  0.2
+#       - Todos os ajustes de cada cliente deverão ser escritos no arquivo de variáveis PARAMETROSBKPBD.
+#       VERSAO:  0.3.1
 #       CRIADO:  12/02/2020
 #	AUTOR: Matheus Martins
 #       REVISAO:  ---
@@ -22,15 +23,23 @@
 #       - Ajustado leitura do fstab para não coletar linhas com /mnt/storage que comecem com comentários
 #	21/02/2020 10:30
 #	- Adicionada validação para casos de backup em pasta de rede
-#	- Adicionado no repositório do github
+#	- Adicionado no repositório para versionamento git/github
+#       16/03/2020 11:20
+#       - Ajustado cabeçalho e corpo de email para melhor informar das notificações de eventos de erros ocorridos
+#       22/03/2020 12:00
+#       - Adicionado webook de notificação do github no Discord da Smartnx
+#       24/03/2020 09:00
+#       - Ajuste nas funções de sendmail e trap error que não estavam exportando o arquivos de PARAMETROSBKPBD
+#       - Ajuste na retenção do backup, removendo assim backups antigos mesmo se o servidor estiver desligado em sua proxima execução
 #=============================================================================================
 
-source PARAMETOSBKPBD
+source PARAMETROSBKPBD
 
 send_mail()
 {
-        echo -e "Verificar status de Backup de $(hostname)" | mail -s "$(hostname) - $DISCOMONTAGEM ERRO NO PROCESSO DE BACKUP. LOG LOCALIZADO EM $LOGBANCO" ${TO[$x]}
-        echo -e "$LOGBANCO: Email de notificação enviado para $TO" >> $LOGBANCO
+        source PARAMETROSBKPBD
+        echo -e "Verificar com urgencia o status de backup do banco de dados de $(hostname) ocorrido em $DATALOG em $DESTINOBASE" | mail -s "$hostname - ERRO NO PROCESSO DE BACKUP... LOG LOCALIZADO EM $LOGBANCO" ${TO[$x]}
+        echo -e "$DATALOG $LOGBANCO: Email de notificação enviado para $TO" >> $LOGBANCO
         exit 0
 }
 
@@ -42,12 +51,12 @@ trap_error()
         exit 0
 }
 trap 'trap_error' 1 2 3 5 6 15 25
+
 #Validar se existem discos a serem montados
 if [ $QTDISCO -eq 0 ];
 then
         echo "OK Nao existem particoes a serem montadas"
         echo "Destino de backup em /dados/backup/PGSQL"
-        DESTINOBASE="/dados/backup/PGSQL"
         DESTINO="/$DESTINOBASE/$DATAHORA"
 elif [ ! -d $DESTINOBASE ];
 then
@@ -62,18 +71,23 @@ else
 fi
 sleep 2
 ESPACODISCO="$( df $DESTINOBASE | sed -E "s/.* (.*)% .*$/\1/g" | tail -n 1)"
+
 #Executando backup
 if [ "$ESPACODISCO" -lt 85 -a -d $DESTINOBASE ] ;
 then
-#Removendo backup antigo
-        /bin/rm -rfv $DESTINO/$DATAREMOVER* && echo "$DATALOG: Removido backup de: $DATAREMOVER" >> $LOGBANCO
 
+#Removendo backup antigo
+        find $DESTINOBASE/ -maxdepth 1 -mtime +$MAXRET -exec rm -rfv "{}" \; >> $LOGBANCO && echo "$DATALOG: Removidos os backups antigos:" >> $LOGBANCO
+        
 #Cria pasta no destino com data/hora
        /bin/mkdir -p "$DESTINO"
 
 #Gerando backup de banco
        echo "$DATALOG: Gerando Backup Base - dbcallcenter em: $DESTINO" >> $LOGBANCO
        /usr/bin/pg_dump -U dbcallcenter $BASE | gzip -9 > $DESTINO/bkp_base_$BASE.sql.gz
+       echo "$DATALOG: Backup criado: $DESTINO/bkp_base_$BASE.sql.gz" >> $LOGBANCO
+
+#Vaccum analyze
        /usr/bin/psql -U dbcallcenter -c "vacuum analyze"; >> $LOGBANCO
 else
         echo "$DATALOG: $PONTOMONTAGEM com menos de 15% de espaço livre." >> $LOGBANCO;
